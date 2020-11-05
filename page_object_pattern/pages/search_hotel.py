9782dbe4831db4f0ca354616a779afe783cd075d
NattheCh <nataliachamerska@gmail.com>
2020-11-05 22:00:55 +0100

    commit logging and loggers

--- page_object_pattern/pages/search_hotel.py
+++ page_object_pattern/pages/search_hotel.py
@@ -1,3 +1,5 @@
+
+from page_object_pattern.pages.locators import SearchHotelLocators
 import logging
 
 
@@ -6,35 +8,26 @@ class SearchHotelPage:
     def __init__(self, driver):
         self.driver = driver
         self.logger = logging.getLogger(__name__)
-        self.search_hotel_span_xpath = "//span[text()='Search by Hotel or City Name']"
-        self.search_hotel_input_xpath = "//div[@id='select2-drop']//input"
-        self.location_match_xpath = "//span[text()='Dubai']"
-        self.check_in_input_name = "checkin"
-        self.check_out_input_name = "checkout"
-        self.travelers_input_id = "travellersInput"
-        self.adult_input_id = "adultInput"
-        self.child_input_id = "childInput"
-        self.search_button_xpath = "//button[text()=' Search']"
 
     def set_city(self, city):
         self.logger.info("Setting city {}".format(city))
-        self.driver.find_element_by_xpath(self.search_hotel_span_xpath).click()
-        self.driver.find_element_by_xpath(self.search_hotel_input_xpath).send_keys(city)
-        self.driver.find_element_by_xpath(self.location_match_xpath).click()
+        self.driver.find_element_by_xpath(SearchHotelLocators.search_hotel_span_xpath).click()
+        self.driver.find_element_by_xpath(SearchHotelLocators.search_hotel_input_xpath).send_keys(city)
+        self.driver.find_element_by_xpath(SearchHotelLocators.location_match_xpath).click()
 
     def set_date_range(self, check_in, check_out):
         self.logger.info("Setting check in {checkin} and check out {checkout} dates".format(checkin=check_in, checkout=check_out))
-        self.driver.find_element_by_name(self.check_in_input_name).send_keys(check_in)
-        self.driver.find_element_by_name(self.check_out_input_name).send_keys(check_out)
+        self.driver.find_element_by_name(SearchHotelLocators.check_in_input_name).send_keys(check_in)
+        self.driver.find_element_by_name(SearchHotelLocators.check_out_input_name).send_keys(check_out)
 
     def set_travelers(self, adults, child):
         self.logger.info("Setting travelers: adults - {adults} and child - {kids}".format(adults=adults, kids=child))
-        self.driver.find_element_by_id(self.travelers_input_id).click()
-        self.driver.find_element_by_id(self.adult_input_id).clear()
-        self.driver.find_element_by_id(self.adult_input_id).send_keys(adults)
-        self.driver.find_element_by_id(self.child_input_id).clear()
-        self.driver.find_element_by_id(self.child_input_id).send_keys(child)
+        self.driver.find_element_by_id(SearchHotelLocators.travelers_input_id).click()
+        self.driver.find_element_by_id(SearchHotelLocators.adult_input_id).clear()
+        self.driver.find_element_by_id(SearchHotelLocators.adult_input_id).send_keys(adults)
+        self.driver.find_element_by_id(SearchHotelLocators.child_input_id).clear()
+        self.driver.find_element_by_id(SearchHotelLocators.child_input_id).send_keys(child)
 
     def perform_search(self):
         self.logger.info("Performing search")
-        self.driver.find_element_by_xpath(self.search_button_xpath).click()
+        self.driver.find_element_by_xpath(SearchHotelLocators.search_button_xpath).click()
--- page_object_pattern/pages/search_results.py
+++ page_object_pattern/pages/search_results.py
@@ -1,3 +1,4 @@
+from page_object_pattern.pages.locators import SearchResultsLocators
 import logging
 
 
@@ -6,11 +7,9 @@ class SearchResultsPage:
     def __init__(self, driver):
         self.driver = driver
         self.logger = logging.getLogger(__name__)
-        self.hotel_names_xpath = "//h4[contains(@class, 'list_title')]//b"
-        self.hotel_prices_xpath = "//div[contains(@class, 'price_tab')]//b"
 
     def get_hotel_names(self):
-        hotels = self.driver.find_elements_by_xpath(self.hotel_names_xpath)
+        hotels = self.driver.find_elements_by_xpath(SearchResultsLocators.hotel_names_xpath)
         self.logger.info("Available hotels: ")
         names = [hotel.get_attribute("textContent") for hotel in hotels]
         for name in names:
@@ -18,7 +17,7 @@ class SearchResultsPage:
         return names
 
     def get_hotel_prices(self):
-        prices = self.driver.find_elements_by_xpath(self.hotel_prices_xpath)
+        prices = self.driver.find_elements_by_xpath(SearchResultsLocators.hotel_prices_xpath)
         hotel_prices = [price.get_attribute("textContent") for price in prices]
         self.logger.info("Prices are:")
         for price in hotel_prices:
